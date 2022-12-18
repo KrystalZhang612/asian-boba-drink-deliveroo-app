7887e7b7f377d334916083d2961ab2f97701d574
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-18 04:54:20 -0800

    last commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 import styles from "../styles/Navbar.module.css"
-import Image from "next/image";
+import Image from "next/legacy/image"; 
 
 const Navbar = () => {
     return (
@@ -33,4 +33,4 @@ const Navbar = () => {
         </div>
     );
 };
-export default Navbar 
+export default Navbar; 
