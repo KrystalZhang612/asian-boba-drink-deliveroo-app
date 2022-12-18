c5da4ca62c6050bd6af654b80139e4265825c200
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-18 05:02:14 -0800

    last commit

--- pages/api/orders/products/index.js
+++ pages/api/orders/products/index.js
@@ -1,6 +1,3 @@
-import dbConnect from "../../../util/mongo";
-import Product from "../../../models/Product";
-
 export default async function handler(req, res) {
     const { method, cookies } = req;
 
